30f159a7a3637c7eee0999939dab3c034d15cbc1
Sippora04 <sippora.toppo95@gmail.com>
2020-11-03 07:35:40 +0530

    Refactored: Generic class to find max

--- main/java/com/testmaximum/Maximum.java
+++ main/java/com/testmaximum/Maximum.java
@@ -1,6 +1,17 @@
 package com.testmaximum;
-
-public class Maximum {
+//Refactor2: Generic class
+public class Maximum<T extends Comparable<T>> {
+	T x, y, z;
+	public Maximum(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z= z;
+	}
+	
+	public T maximum() {
+		return Maximum.maximum(x, y, z);
+	}
+	
 	//Refactor1: Generic method to find maximum
 	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
 		T max = x;
