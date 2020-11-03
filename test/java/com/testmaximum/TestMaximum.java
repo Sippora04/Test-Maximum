bd377810c449dfe9665f1543224ee50200700b49
Sippora04 <sippora.toppo95@gmail.com>
2020-11-03 06:39:55 +0530

    Added: 3 floating number and find max

--- test/java/com/testmaximum/TestMaximum.java
+++ test/java/com/testmaximum/TestMaximum.java
@@ -19,7 +19,7 @@ public class TestMaximum {
 	}
 	
 	@Test
-	public void testIntegerMaxWithMaxAtSecondPosition() {
+	public void testIntegerMax_MaxAtSecondPosition() {
 		Integer max = Maximum.findIntegerMaximum(63, 99, 42);
 		boolean flag;
 		if (max.equals(99)) {
@@ -31,10 +31,46 @@ public class TestMaximum {
 	}
 
 	@Test
-	public void testIntegerMaxWithMaxAtThirdPosition() {
+	public void testIntegerMax_MaxAtThirdPosition() {
 		Integer max = Maximum.findIntegerMaximum(76, 23, 50);
 		boolean flag;
 		if (max.equals(76)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testFloatMax_MaxAtFirstPosition() {
+		Float max = Maximum.findFloatMaximum(8.4f, 4.6f, 7.5f);
+		boolean flag;
+		if (max.equals(8.4f)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testFloatMaxWithMaxAtSecondPosition() {
+		Float max = Maximum.findFloatMaximum(4.6f, 9.4f, 1.5f);
+		boolean flag;
+		if (max.equals(9.4f)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		assertEquals(true, flag);
+	}
+
+	@Test
+	public void testFloatMaxWithMaxAtThirdPosition() {
+		Float max = Maximum.findFloatMaximum(1.5f, 4.6f, 98.4f);
+		boolean flag;
+		if (max.equals(98.4f)) {
 			flag = true;
 		} else {
 			flag = false;
