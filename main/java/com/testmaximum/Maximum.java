9ab493482ac8e189fb53c93a5cfac7d47cc5a154
Sippora04 <sippora.toppo95@gmail.com>
2020-11-03 08:09:47 +0530

    Added: printMax Generic Method

--- main/java/com/testmaximum/Maximum.java
+++ main/java/com/testmaximum/Maximum.java
@@ -8,7 +8,13 @@ public class Maximum<T extends Comparable<T>> {
 	public static <T extends Comparable<T>> T maximum(T ... values) {
 		Arrays.sort(values);
 		T max = values[values.length-1];
+		printMax(max);
 		return max;
+	}
+
+	public static<T> void printMax(T max) {
+		// TODO Auto-generated method stub
+		System.out.println("Maximum: " + max + "\n");
 	}
 }
 
--- test/java/com/testmaximum/TestMaximum.java
+++ test/java/com/testmaximum/TestMaximum.java
@@ -40,6 +40,17 @@ public class TestMaximum {
 	}
 	
 	@Test
+	public void testIntegerMax_MaxAtFourParameters() {
+		boolean flag;
+		if ((Maximum.maximum(63, 99, 42, 1)).equals(99)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		assertEquals(true, flag);
+	}
+	
+	@Test
 	public void testFloatMax_MaxAtFirstPosition() {
 		boolean flag;
 		if ((Maximum.maximum(8.4f, 4.6f, 7.5f)).equals(8.4f)) {
@@ -73,6 +84,18 @@ public class TestMaximum {
 	}
 	
 	@Test
+	public void testFloatMax_MaxAtFourParameter() {
+		boolean flag;
+		if ((Maximum.maximum(3.2f, 5.6f, 1.5f,10.0f)).equals(10.0f)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		assertEquals(true, flag);
+	}
+	
+	
+	@Test
 	public void testStringMax_MaxAtFirstPosition() {
 		boolean flag;
 		if ((Maximum.maximum("Peach", "Apple", "Banana")).equals("Peach")) {
@@ -95,9 +118,20 @@ public class TestMaximum {
 	}
 
 	@Test
-	public void testStringMaxWithMaxAtThirdPosition() {
+	public void testStringMax_MaxAtThirdPosition() {
 		boolean flag;
 		if ((Maximum.maximum("Apple", "Banana", "Peach")).equals("Peach")) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		assertEquals(true, flag);
+	}
+	
+	@Test
+	public void testStringMax_MaxAtFourParameters() {
+		boolean flag;
+		if ((Maximum.maximum("Apple", "Banana", "Peach", "Orange")).equals("Peach")) {
 			flag = true;
 		} else {
 			flag = false;
