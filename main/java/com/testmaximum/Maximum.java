af04a7f1eb55ca316eca40a1ab23da76d7518b0f
Sippora04 <sippora.toppo95@gmail.com>
2020-11-03 07:49:05 +0530

    Added: extend the max method to take more than 3 parameters

--- main/java/com/testmaximum/Maximum.java
+++ main/java/com/testmaximum/Maximum.java
@@ -1,32 +1,14 @@
 package com.testmaximum;
-//Refactor2: Generic class
-public class Maximum<T extends Comparable<T>> {
-	T x, y, z;
-	public Maximum(T x, T y, T z) {
-		this.x = x;
-		this.y = y;
-		this.z= z;
-	}
-	
-	public T maximum() {
-		return Maximum.maximum(x, y, z);
-	}
-	
-	//Refactor1: Generic method to find maximum
-	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-		T max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
-		printMax(x, y, z, max);
-			return max;
-	}
 
-	public static <T> void printMax(T x, T y, T z, T max) {
-		System.out.printf("Maximum of " + x + ", " + y + " and " + z + " is " + max + "\n");
+import java.util.Arrays;
+
+//Generic class
+public class Maximum<T extends Comparable<T>> {
+	//Any number of arguments
+	public static <T extends Comparable<T>> T maximum(T ... values) {
+		Arrays.sort(values);
+		T max = values[values.length-1];
+		return max;
 	}
 }
 
--- test/java/com/testmaximum/TestMaximum.java
+++ test/java/com/testmaximum/TestMaximum.java
@@ -8,9 +8,8 @@ public class TestMaximum {
 
 	@Test
 	public void testIntegerMax_MaxAtFirstPosition() {
-		Integer max = Maximum.maximum(5, 0, 1);
 		boolean flag;
-		if (max.equals(5)) {
+		if ((Maximum.maximum(5, 0, 1)).equals(5)) {
 			flag = true;
 		} else {
 			flag = false;
@@ -20,9 +19,8 @@ public class TestMaximum {
 	
 	@Test
 	public void testIntegerMax_MaxAtSecondPosition() {
-		Integer max = Maximum.maximum(63, 99, 42);
 		boolean flag;
-		if (max.equals(99)) {
+		if ((Maximum.maximum(63, 99, 42)).equals(99)) {
 			flag = true;
 		} else {
 			flag = false;
@@ -32,9 +30,8 @@ public class TestMaximum {
 
 	@Test
 	public void testIntegerMax_MaxAtThirdPosition() {
-		Integer max = Maximum.maximum(76, 23, 50);
 		boolean flag;
-		if (max.equals(76)) {
+		if ((Maximum.maximum(76, 23, 50)).equals(76)) {
 			flag = true;
 		} else {
 			flag = false;
@@ -44,9 +41,8 @@ public class TestMaximum {
 	
 	@Test
 	public void testFloatMax_MaxAtFirstPosition() {
-		Float max = Maximum.maximum(8.4f, 4.6f, 7.5f);
 		boolean flag;
-		if (max.equals(8.4f)) {
+		if ((Maximum.maximum(8.4f, 4.6f, 7.5f)).equals(8.4f)) {
 			flag = true;
 		} else {
 			flag = false;
@@ -56,9 +52,8 @@ public class TestMaximum {
 	
 	@Test
 	public void testFloatMax_MaxAtSecondPosition() {
-		Float max = Maximum.maximum(4.6f, 9.4f, 1.5f);
 		boolean flag;
-		if (max.equals(9.4f)) {
+		if ((Maximum.maximum(4.6f, 9.4f, 1.5f)).equals(9.4f)) {
 			flag = true;
 		} else {
 			flag = false;
@@ -68,9 +63,8 @@ public class TestMaximum {
 
 	@Test
 	public void testFloatMax_MaxAtThirdPosition() {
-		Float max = Maximum.maximum(1.5f, 4.6f, 98.4f);
 		boolean flag;
-		if (max.equals(98.4f)) {
+		if ((Maximum.maximum(1.5f, 4.6f, 98.4f)).equals(98.4f)) {
 			flag = true;
 		} else {
 			flag = false;
@@ -80,9 +74,8 @@ public class TestMaximum {
 	
 	@Test
 	public void testStringMax_MaxAtFirstPosition() {
-		String max = Maximum.maximum("Peach", "Apple", "Banana");
 		boolean flag;
-		if (max.equals("Peach")) {
+		if ((Maximum.maximum("Peach", "Apple", "Banana")).equals("Peach")) {
 			flag = true;
 		} else {
 			flag = false;
@@ -92,9 +85,8 @@ public class TestMaximum {
 
 	@Test
 	public void testStringMax_MaxAtSecondPosition() {
-		String max = Maximum.maximum("Apple", "Peach", "Banana");
 		boolean flag;
-		if (max.equals("Peach")) {
+		if ((Maximum.maximum("Apple", "Peach", "Banana")).equals("Peach")) {
 			flag = true;
 		} else {
 			flag = false;
@@ -104,9 +96,8 @@ public class TestMaximum {
 
 	@Test
 	public void testStringMaxWithMaxAtThirdPosition() {
-		String max = Maximum.maximum("Apple", "Banana", "Peach");
 		boolean flag;
-		if (max.equals("Peach")) {
+		if ((Maximum.maximum("Apple", "Banana", "Peach")).equals("Peach")) {
 			flag = true;
 		} else {
 			flag = false;
